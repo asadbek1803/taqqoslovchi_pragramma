383365747a9cce5ed5bfbab0df69570597911b18
Asadbek <97333773+asadbek1803@users.noreply.github.com>
2022-01-15 09:51:49 +0500

    Add files via upload

---
+++ untitled1.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jan  5 11:26:12 2022
+
+@author: User 4
+"""
+
+# a = float(input("Birinchi sonni kiriting: "))
+# b = float(input("Ikkinchi sonni kiriting: "))
+# if a>b:
+#     print(f"{a}>{b}")
+# elif a<b:
+#     print(f"{a}<{b}")
+# else:
+#     print(f"{a}={b}")
+    
+
